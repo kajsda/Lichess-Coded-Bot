8254aaaa066bb33db78a265546503bb731cc9b21
KingsHollyCross <105532655+KingsHollyCross@users.noreply.github.com>
2022-07-31 07:27:13 +0530

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -26,7 +26,7 @@ class Conversation:
             self.send_reply(line, "Waiting 60 seconds...")
         elif cmd == "name":
             name = game.me.name
-            self.send_reply(line, f"{name} running {self.engine.name()} (lichess-bot v{self.version})")
+            self.send_reply(line, f"{name} running {self.engine.name()} (Botli")
         elif cmd == "howto":
             self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
         elif cmd == "eval" and line.room == "spectator":
